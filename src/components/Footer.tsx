d0e7a917a1547a52de6f099b8eb7082a817d5d3a
Jon <152871391+jsmaxi@users.noreply.github.com>
2024-10-05 19:59:05 +0300

    Header footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
+
+import logo from "../../images/shogun_logo.png";
+
 export function Footer() {
+  const year = new Date().getFullYear();
   return (
-    <div className="flex items-center justify-between px-4 py-2 max-w-screen-xl mx-auto w-full flex-wrap">
-      <h1 className="display">Shogun</h1>
-    </div>
+    <footer className="flex items-center justify-between px-4 py-2 max-w-screen-xl mx-auto w-full flex-wrap">
+      <div>
+        <Image src={logo} alt="Logo" quality={100} width={70} height={70} placeholder="blur" />
+        <strong>SHOGUN</strong>
+      </div>
+      <p>
+        <i>© {year}. All rights reserved.</i>
+      </p>
+    </footer>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
 import { WalletSelector } from "./WalletSelector";
 
+import Image from "next/image";
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
+import logo from "../../images/shogun_logo.png";
+
 export function Header() {
   return (
     <div className="flex items-center justify-between px-4 py-2 max-w-screen-xl mx-auto w-full flex-wrap">
-      <h1 className="display">Shogun</h1>
-
+      <div>
+        <Image src={logo} alt="Logo" quality={100} width={70} height={70} placeholder="blur" />
+        <strong>SHOGUN</strong>
+      </div>
+      <Link href="/">
+        <Button className="bg-indigo-500 hover:bg-gray-300 focus-visible:outline-indigo-600 w-32">HOME</Button>
+      </Link>
       <div className="flex gap-2 items-center flex-wrap">
         <WalletSelector />
       </div>
