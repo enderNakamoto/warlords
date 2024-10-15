4b36a1ee7b2d0bbbbae46860ca71ba20ebdcfea2
Jon <152871391+jsmaxi@users.noreply.github.com>
2024-10-15 15:57:01 +0300

    Firebase integration to load rankings

---
+++ src/utils/firebaseClient.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
+  
+const app = initializeApp(firebaseConfig);
+const firebaseDb = getFirestore(app);
+
+export default firebaseDb;
